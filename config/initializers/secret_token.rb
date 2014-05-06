e5a49a8d6d8e569d418229328aac1574112c458e
Joe21 <joej21@gmail.com>
2014-05-06 19:17:20 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedbackMvp::Application.config.secret_token = 'd972b08cef552e8880521970bac9e965d5f80ca00250b5089c6aebb3719cfb9e472ca1fe161101f818a21dc8dc2171f63cddb6b9042b197e9820e1d2be69e067'
