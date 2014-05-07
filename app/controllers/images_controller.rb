9b679c875badff5689185dab9a99014f87090762
Joe21 <joej21@gmail.com>
2014-05-06 22:15:38 -0400

    running out of time, audible to link image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,11 @@
 class ImagesController < ApplicationController
 	def index
-		feedback = Feedback.find(params[:feedback_id])
-		@images = feedback.images
+		@feedback = Feedback.find(params[:feedback_id])
+		@images = @feedback.images
+	end
+
+	def new
+		@feedback = Feedback.find(params[:feedback_id])
+		@image = Image.new
 	end
 end
